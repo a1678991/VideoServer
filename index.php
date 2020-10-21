6d82eb1f249cbe364cc7664a4d1f4474fed4d8a7
ue8d <webmaster@tomoki12.com>
2020-10-21 21:37:34 +0900

    トップページ追加

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8" />
+    <title>Play</title>
+  </head>
+  <body style="background-color:white">
+    <p><a href="./01/play.php">ビデオリスト</a></p>
+  </body>
+</html>
