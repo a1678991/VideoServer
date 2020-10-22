c8df673bff92e06757369b6238bd00ffbd5f891c
ue8d <webmaster@tomoki12.com>
2020-10-23 00:22:15 +0900

    HLS対応させた動画一覧を表示出来るように

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
   <html>
   <head>
     <meta charset="utf-8" />
-    <title>Play</title>
+    <title>いんでっくす</title>
   </head>
   <body style="background-color:white">
-    <p><a href="./01/play.php">サンプルビデオ</a></p>
     <p><a href="./02/vod.php">VOD sample</a></p>
     <?php
       foreach(glob('video/{*.mp4}',GLOB_BRACE) as $file){
@@ -18,5 +17,15 @@
       }
     ?>
     <br>
+    <?php
+      foreach(glob('workspace/{*.m3u8}',GLOB_BRACE) as $file){
+        if(is_file($file)){
+          //print(htmlspecialchars($file)."<br>");
+  ?>
+          <a href="./02/vod.php?videoPass=<?php print($file) ?>&videoName=<?php print((substr($file, 10, -5))); ?>"><?php print(substr($file,10,-5)); ?></a>
+  <?php
+        }
+      }
+    ?>
   </body>
 </html>
