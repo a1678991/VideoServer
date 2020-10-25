f5b17d7340eda5dabcfaaa2c2c08323776525f09
ue8d <webmaster@tomoki12.com>
2020-10-25 21:42:38 +0900

    ヘッダーの追加＆CSS追加

--- index.php
+++ index.php
@@ -3,30 +3,49 @@
   <head>
     <meta charset="utf-8" />
     <title>いんでっくす</title>
+    <link rel="stylesheet" type="text/css" href="./CSS/index.css">
   </head>
   <body style="background-color:white">
-    <p>サンプルムービー</p>
-    <?php
-      foreach(glob('video/{*.mp4}',GLOB_BRACE) as $file){
-        if(is_file($file)){
+    <header>
+      <h1 class="header">
+          <a class="header" href="/">ue8d's Videos</a>
+      </h1 class="header">
+      <nav class="pc-nav">
+          <ul class="header">
+              <li class="header"><a class="header" href="#">ABOUT</a></li>
+              <li class="header"><a class="header" href="#">CONTACT</a></li>
+          </ul>
+      </nav>
+    </header>
+
+    <div class = "main">
+      <p>サンプルムービー</p>
+      <?php
+        foreach(glob('video/{*.mp4}',GLOB_BRACE) as $file){
+          if(is_file($file)){
+              //print(htmlspecialchars($file)."<br>");
+      ?>
+              <a href="./01/play.php?videoPass=<?php print($file) ?>&videoName=<?php print((substr($file, 6, -4))); ?>"><?php print(substr($file,6)); ?></a><br>
+      <?php
+          }
+        }
+      ?>
+      <br>
+      <p>VOD一覧</p>
+      <?php
+        foreach(glob('workspace/{*.m3u8}',GLOB_BRACE) as $file){
+          if(is_file($file)){
             //print(htmlspecialchars($file)."<br>");
-    ?>
-            <a href="./01/play.php?videoPass=<?php print($file) ?>&videoName=<?php print((substr($file, 6, -4))); ?>"><?php print(substr($file,6)); ?></a><br>
-    <?php
+      ?>
+            <a href="./02/vod.php?videoPass=<?php print($file) ?>&videoName=<?php print((substr($file, 10, -5))); ?>"><?php print(substr($file,10,-5)); ?></a>
+      <?php
+          }
         }
-      }
-    ?>
-    <br>
-    <p>VOD一覧</p>
-    <?php
-      foreach(glob('workspace/{*.m3u8}',GLOB_BRACE) as $file){
-        if(is_file($file)){
-          //print(htmlspecialchars($file)."<br>");
-    ?>
-          <a href="./02/vod.php?videoPass=<?php print($file) ?>&videoName=<?php print((substr($file, 10, -5))); ?>"><?php print(substr($file,10,-5)); ?></a>
-    <?php
-        }
-      }
-    ?>
+      ?>
+    </div>
+
+    <footer>
+      <!-- フッター -->
+    </footer>
   </body>
 </html>
