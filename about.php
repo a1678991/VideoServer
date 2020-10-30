50691834778e868c2c2ce96256e7bacd4de902be
ue8d <webmaster@tomoki12.com>
2020-10-30 15:26:18 +0900

    aboutページ作成

---
+++ about.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+    <meta charset="utf-8" />
+    <title>about</title>
+    <link rel="stylesheet" type="text/css" href="./CSS/index.css">
+  </head>
+  <body style="background-color:white" class="index">
+    <!-- ヘッダー読み込み -->
+    <?php include_once "./header.php" ?>
+
+    <div class="main">
+      <p>about</p>
+      <p>HDD内の動画をブラウザ上で閲覧できるようにしたサイトです。</p>
+    </div>
+
+    <footer>
+      <p>© All rights reserved by ue8d.</p>
+    </footer>
+  </body>
+</html>
