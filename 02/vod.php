7ee7d2914817d616065140c986c2441a0d8e9979
ue8d <webmaster@tomoki12.com>
2020-10-30 15:08:43 +0900

    ソースコードの可読性UP、CSS適用のためのコード追加

--- 02/vod.php
+++ 02/vod.php
@@ -1,23 +1,19 @@
 <DOCTYPE html>
-<html lang="en" class="">
+<html lang="ja">
     <head>
         <title>
-            VOD ”<?php
-                if(isset($_GET["videoName"])){
-                    print($_GET["videoName"]);
-                }else {
-                    echo "読み込めませんでした";
-                }
-            ?>”
+            VOD ”<?php if(isset($_GET["videoName"])) { print($_GET["videoName"]);}else {echo "読み込めませんでした";} ?>”
         </title>
+
         <link href="./video-js.min.css" rel="stylesheet">
         <link href="../CSS/index.css" rel="stylesheet">
+
         <script src="./hls.min.js"></script>
         <script type="text/javascript">
             var source = "<?php echo $_GET['videoPass']; ?>";
         </script>
     </head>
-    <body>
+    <body style="background-color:white" class="vod">
         <?php include_once "../header.php" ?>
         <p>
             <?php
@@ -28,31 +24,33 @@
                 }
             ?>
         </p>
-        <video id="video" class="video-js vjs-default-skin" width="640" height="480" controls>
-        <script type="text/javascript">
-            // 変換した動画ファイルを指定します。
-            source = '../' + source;
-            var ua = navigator.userAgent;
-        　　// スマホからのアクセスか、それ以外のアクセスかを判断
-            if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
-                // iOS
-                document.write('<source src=' + source + ' type="application/x-mpegURL">');
-                document.write('</video>');
+        <div class="vod">
+            <video id="video" class="video-js vjs-default-skin vjs-default-centered" width="640" height="480" controls>
+            <script type="text/javascript">
+                // 変換した動画ファイルを指定します。
+                source = '../' + source;
+                var ua = navigator.userAgent;
+            　　// スマホからのアクセスか、それ以外のアクセスかを判断
+                if (ua.indexOf('iPhone') > 0 || ua.indexOf('iPad') > 0 || ua.indexOf('Android') > 0) {
+                    // iOS
+                    document.write('<source src=' + source + ' type="application/x-mpegURL">');
+                    document.write('</video>');
 
-            }else{
-                // OTHER
-                document.write('</video>');
-                if(Hls.isSupported()) {
-                    var video = document.getElementById('video');
-                    var hls = new Hls();
-                    hls.loadSource(source);
-                    hls.attachMedia(video);
-                    hls.on(Hls.Events.MANIFEST_PARSED,function() {
-                        video.pause();
-                    });
+                }else{
+                    // OTHER
+                    document.write('</video>');
+                    if(Hls.isSupported()) {
+                        var video = document.getElementById('video');
+                        var hls = new Hls();
+                        hls.loadSource(source);
+                        hls.attachMedia(video);
+                        hls.on(Hls.Events.MANIFEST_PARSED,function() {
+                            video.pause();
+                        });
+                    }
                 }
-            }
-        </script>
+            </script>
+        </div>
         <br>
         <a href="../index.php">トップへ戻る</a>
 
